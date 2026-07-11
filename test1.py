2abe8985ff98f9f75c8d2fcaba80eec26645193d
Allan Acosta Porras <aacostap1990@gmail.com>
2026-07-11 08:57:24 -0600

    Refactor.

--- test1.py
+++ test1.py
@@ -1,9 +1,20 @@
 from core.llm_client import FreeClaudeCodeClient
+from core.chroma_db import RAGConfig, PDFProcessor
+from helpers.constants import RESPUESTAS_FOLDER
+import os
+config = RAGConfig()
+
+pdf_processor = PDFProcessor(config=config)
+
+docs = pdf_processor.process_all_pdfs()
 
 client = FreeClaudeCodeClient()
 
 respuesta = client.ask(
-    "¿Cual es la mejor forma de aprender a programar agentes?"
+    "¿Cual es la cantidad de argumentos recomendada para una funcion?",
+    system_prompt = docs
 )
 
-print(respuesta)
+
+with open(os.path.join(RESPUESTAS_FOLDER,"output.md"), "w", encoding="utf-8") as f:
+    f.write(respuesta)
